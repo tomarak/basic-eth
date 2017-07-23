26a1c177dc84925552c1cc028d74c22a3d8d5bf1
Anuj Tomar <ankuto@gmail.com>
2017-07-22 20:07:39 -0700

    Add comments

--- app/discovery.py
+++ app/discovery.py
@@ -7,15 +7,29 @@ from crypto import keccak256
 from secp256k1 import PrivateKey
 from ipaddress import ip_address
 
+
 class EndPoint(object):
-    def __init__(self, address, udpPort, tcpPort):
+    """Ping communication end point class."""
+
+    def __init__(self, address, udp_port, tcp_port):
+        """Initialize end point class."""
+        # The address is passed to the ip_address package in order
+        # to later convert the address from dot format to binary format
         self.address = ip_address(address)
-        self.udpPort = udpPort
-        self.tcpPort = tcpPort
+        self.udp_port = udp_port
+        self.tcp_port = tcp_port
 
     def pack(self):
+        """
+        Prepare the object for encoding.
+
+        - The IP address is converted to a BE encoded 4-byte address
+        - RLP specification says "Ethereum integers must be represented in big
+        endian binary form with no leading zeroes"
+        - Port data types are also unsigned 16-bit integers
+        """
         return [
             self.address.packed,
-            struct.pack(">H", self.udpPort),
-            struct.pack(">H", self.tcpPort),
+            struct.pack(">H", self.udp_port),
+            struct.pack(">H", self.tcp_port),
         ]
