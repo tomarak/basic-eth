211d15149ad4e05c2b1159939805246998a2e3d4
Anuj Tomar <ankuto@gmail.com>
2017-07-22 22:40:34 -0700

    Add script to run app

---
+++ app/send_ping.py
@@ -0,0 +1,11 @@
+from discovery import Endpoint, PingNode, PingServer
+
+my_endpoint = EndPoint(u'192.168.1.192', 30303, 30303)
+their_endpoint = EndPoint(u'127.0.0.1', 30303, 30303)
+
+server = PingServer(my_endpoint)
+
+listen_thread = server.udp_listen()
+listen_thread.start()
+
+server.ping(their_endpoint)
