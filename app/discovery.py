c7b72eb35b32b564698e4463450445b859afe485
Anuj Tomar <ankuto@gmail.com>
2017-07-22 19:22:44 -0700

    Add endpoint class

---
+++ app/discovery.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+import time
+import struct
+import rlp
+from crypto import keccak256
+from secp256k1 import PrivateKey
+from ipaddress import ip_address
+
+class EndPoint(object):
+    def __init__(self, address, udpPort, tcpPort):
+        self.address = ip_address(address)
+        self.udpPort = udpPort
+        self.tcpPort = tcpPort
+
+    def pack(self):
+        return [self.address.packed,
+        struct.pack(">H", self.udpPort),
+        struct.pack(">H", self.tcpPort)]
